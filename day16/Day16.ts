fd4cbd6ccfeccae4a9cacf4238454d0482b4d241
Todd Southen <tsouthen@gmail.com>
2021-12-17 09:16:59 -0800

    Code cleanup.

--- day16/Day16.ts
+++ day16/Day16.ts
@@ -6,6 +6,11 @@ class Packet {
   literalValue = 0;
   subPackets = new Array<Packet>();
 
+  constructor(data: string = "") {
+    if (data.length > 0)
+      this.parse(data, 0);
+  }
+
   get isValid() {
     return this.version !== 0 || this.typeId === 4 || this.subPackets.length > 0;
   }
@@ -15,10 +20,10 @@ class Packet {
       return -1;
     let currPos = startPos;
     this.version = Number.parseInt(data.substr(currPos, 3), 2);
-    // console.log(`version ${this.version}`);
     currPos += 3;
     this.typeId = Number.parseInt(data.substr(currPos, 3), 2);
     currPos += 3;
+
     if (this.typeId === 4) {
       let firstBit = "";
       let binaryLiteral = "";
@@ -27,47 +32,41 @@ class Packet {
         binaryLiteral = binaryLiteral + data.substr(currPos, 4);
         currPos += 4;
       } while (firstBit === "1");
-
       this.literalValue = Number.parseInt(binaryLiteral, 2);
-    } else { // subpackets
-      const lengthType = data[currPos++];
-      if (lengthType === "0") { //length of subpacket bits
-        const lengthBits = data.substr(currPos, 15);
-        currPos += 15;
-        const numSubPacketBits = Number.parseInt(lengthBits, 2);
-        const subPacketData = data.substr(currPos, numSubPacketBits);
-        currPos += numSubPacketBits;
-        let subIdx = 0;
-        let packet = new Packet();
-        do {
-          [packet, subIdx] = Packet.newPacket(subPacketData, subIdx);
-          if (packet.isValid) {
-            this.subPackets.push(packet);
-          }
-        } while (packet.isValid);
-      } else { //num subpackets
-        const numSubsBits = data.substr(currPos, 11);
-        currPos += 11;
-        const numSubs = Number.parseInt(numSubsBits, 2);
-        let packet = new Packet();
-        Array.from({ length: numSubs }).forEach(_ => {
-          [packet, currPos] = Packet.newPacket(data, currPos);
-          this.subPackets.push(packet);
-        });
-      }
+      return currPos;
     }
+
+    // subpackets
+    const lengthType = data[currPos++];
+    if (lengthType === "0") { // length of subpacket bits
+      const lengthBits = data.substr(currPos, 15);
+      currPos += 15;
+      const numSubPacketBits = Number.parseInt(lengthBits, 2);
+      const subPacketData = data.substr(currPos, numSubPacketBits);
+      currPos += numSubPacketBits;
+      let subIdx = 0;
+      do {
+        const packet = new SubPacket(subPacketData, subIdx);
+        subIdx = packet.endPos;
+        if (packet.isValid) {
+          this.subPackets.push(packet);
+        } else {
+          break;
+        }
+      } while (true);
+      return currPos;
+    }
+
+    // num subpackets
+    const numSubsBits = data.substr(currPos, 11);
+    currPos += 11;
+    const numSubs = Number.parseInt(numSubsBits, 2);
+    Array.from({ length: numSubs }).forEach(_ => {
+      const packet = new SubPacket(data, currPos);
+      currPos = packet.endPos;
+      this.subPackets.push(packet);
+    });
     return currPos;
-  }
-
-  get versionSum() {
-    const sum: number = this.subPackets.reduce((acc, sp) => acc + sp.versionSum, this.version);
-    return sum;
-  }
-
-  static newPacket(data: string, startPos: number): [Packet, number] {
-    const packet = new Packet();
-    const endPos = packet.parse(data, startPos);
-    return [packet, endPos];
   }
 
   static typeIdMap: { [key: number]: (packet: Packet) => number } = {
@@ -75,21 +74,35 @@ class Packet {
     1: packet => packet.subPackets.reduce((acc, sp) => acc * sp.calculatedValue, 1),
     2: packet => Math.min(...packet.subPackets.map(sp => sp.calculatedValue)),
     3: packet => Math.max(...packet.subPackets.map(sp => sp.calculatedValue)),
+    4: packet => packet.literalValue,
     5: packet => packet.subPackets[0].calculatedValue > packet.subPackets[1].calculatedValue ? 1 : 0,
     6: packet => packet.subPackets[0].calculatedValue < packet.subPackets[1].calculatedValue ? 1 : 0,
     7: packet => packet.subPackets[0].calculatedValue === packet.subPackets[1].calculatedValue ? 1 : 0,
   };
 
   get calculatedValue() {
-    if (this.typeId === 4)
-      return this.literalValue;
-
     return Packet.typeIdMap[this.typeId](this);
+  }
+
+  get versionSum() {
+    const sum: number = this.subPackets.reduce((acc, sp) => acc + sp.versionSum, this.version);
+    return sum;
+  }
+}
+
+class SubPacket extends Packet {
+  endPos = 0;
+
+  constructor(data: string, startPos: number) {
+    super();
+    if (data.length > 0)
+      this.endPos = this.parse(data, startPos);
   }
 }
 
 class Day16 {
   data = "";
+  packet: Packet;
 
   hexMap: { [key: string]: string } = {
     "0": "0000",
@@ -115,18 +128,17 @@ class Day16 {
     this.data = Array.from(contents).map((char: string) => this.hexMap[char]).join("");
   }
 
-  partOne() {
-    console.log("Part 1");
+  constructor() {
     this.loadData("day16/day16.txt");
-    const [packet] = Packet.newPacket(this.data, 0);
-    console.log(`version sum: ${packet.versionSum}`);
+    this.packet = new Packet(this.data);
+  }
+
+  partOne() {
+    console.log(`Part 1 - version sum: ${this.packet.versionSum}`);
   }
 
   partTwo() {
-    console.log("Part 2");
-    this.loadData("day16/day16.txt");
-    const [packet] = Packet.newPacket(this.data, 0);
-    console.log(`calculated value: ${packet.calculatedValue}`);
+    console.log(`Part 2 - calculated value: ${this.packet.calculatedValue}`);
   }
 
   static go() {
